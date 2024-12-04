1b9b4499a033ca003d4c15e4b1af03ba8cef398a
Monark Basak <monarkb01@gmail.com>
2024-12-03 21:23:31 -0500

    Complete Day 3 Part 2.

--- day-3/main.go
+++ day-3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -18,27 +19,91 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	var sum = 0
+	var sumPart1 = 0
+	var sumPart2 = 0
+	var textFile bytes.Buffer
 	for scanner.Scan() {
-		currLine := scanner.Text()
-		pattern := regexp.MustCompile(`mul\((-?\d+,-?\d+)\)`)
-		matches := pattern.FindAllStringSubmatch(currLine, -1)
+		textFile.WriteString(scanner.Text())
+	}
+	currLine := textFile.String()
 
-		for _, match := range matches {
-			tokenize := strings.Split(match[1], ",")
-			left, right := tokenize[0], tokenize[1]
-			parsedLeftVal, err := strconv.Atoi(left)
-			if err != nil {
-				log.Fatalln("Failed to parse string. Err: ", err)
-			}
-			parsedRightVal, err := strconv.Atoi(right)
-			if err != nil {
-				log.Fatalln("Failed to parse string. Err: ", err)
-			}
+	// create regex patterns
+	mulPattern := regexp.MustCompile(`mul\((-?\d+,-?\d+)\)`)
+	doPattern := regexp.MustCompile(`do\(\)`)
+	dontPattern := regexp.MustCompile(`don't\(\)`)
 
-			sum += parsedLeftVal * parsedRightVal
+	matchesStr := mulPattern.FindAllStringSubmatch(currLine, -1)
+	matches := mulPattern.FindAllStringSubmatchIndex(currLine, -1)
+	doIndexes := doPattern.FindAllStringSubmatchIndex(currLine, -1)
+	dontIndexes := dontPattern.FindAllStringSubmatchIndex(currLine, -1)
+
+	// create index boundaries where muls should be skipped
+	skipArr := [][]int{}
+
+	for _, dont := range dontIndexes {
+		var boundary = []int{}
+		boundary = append(boundary, dont[0])
+		for _, do := range doIndexes {
+			if do[0] > dont[0] {
+				boundary = append(boundary, do[0])
+				break
+			}
+		}
+
+		if len(boundary) == 1 {
+			boundary = append(boundary, int(^uint(0)>>1))
+		}
+		skipArr = append(skipArr, boundary)
+	}
+
+	var filteredMatches = map[int]string{}
+	for _, match := range matches {
+		valid := true
+		for _, boundary := range skipArr {
+			if boundary[0] <= match[0] && match[0] <= boundary[1] {
+				valid = false
+			}
+		}
+		if valid {
+			_, exists := filteredMatches[match[0]]
+			if !exists {
+				filteredMatches[match[0]] = currLine[match[2]:match[3]]
+			}
 		}
 	}
 
-	fmt.Println("Sum: ", sum)
+	// part 1
+	for _, match := range matchesStr {
+		tokenize := strings.Split(match[1], ",")
+		left, right := tokenize[0], tokenize[1]
+		parsedLeftVal, err := strconv.Atoi(left)
+		if err != nil {
+			log.Fatalln("Failed to parse string. Err: ", err)
+		}
+		parsedRightVal, err := strconv.Atoi(right)
+		if err != nil {
+			log.Fatalln("Failed to parse string. Err: ", err)
+		}
+
+		sumPart1 += parsedLeftVal * parsedRightVal
+	}
+
+	// part 2
+	for _, value := range filteredMatches {
+		tokenize := strings.Split(value, ",")
+		left, right := tokenize[0], tokenize[1]
+		parsedLeftVal, err := strconv.Atoi(left)
+		if err != nil {
+			log.Fatalln("Failed to parse string. Err: ", err)
+		}
+		parsedRightVal, err := strconv.Atoi(right)
+		if err != nil {
+			log.Fatalln("Failed to parse string. Err: ", err)
+		}
+
+		sumPart2 += parsedLeftVal * parsedRightVal
+	}
+
+	fmt.Println("Sum Part 1: ", sumPart1)
+	fmt.Println("Sum Part 2: ", sumPart2)
 }
